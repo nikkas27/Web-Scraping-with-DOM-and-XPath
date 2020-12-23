15ae1267261bd299693814bc77997fa297bc15fe
Nisarg Vadodariya <nisarg72227@gmail.com>
2020-12-23 08:36:51 -0500

    Update FetchingWeb.py

--- FetchingWeb.py
+++ FetchingWeb.py
@@ -17,8 +17,6 @@ content=urllib.request.urlopen("https://www.infoplease.com/homework-help/history
 # Using the BeautifulSoup
 webpg = BeautifulSoup(content, 'lxml')
 
-#print(webpg.prettify())
-
 print("--------------------------------------------------------------------------------------------------------------------------")
 
 # Searching for all the name of the President and Date Addressed present in the <a> tags in <dt> ....
@@ -42,25 +40,18 @@ for link in webpg.find_all('dt'):
 
 
     name = link.find('a').text   #Searching for <a>tags which contains the addressed text...
-    # print("Name and date:", name)
-
+    
     n.append(name)
-    # print("Full name",n)
-
     fs = name.split("(")   #Separating Name from Date
     print("Name:",fs[0])
-
 
     excep = ["Opening Remarks", "Military Sacrifices", "Economic Agenda", "A Hopeful Society"]  #Removing some of the unnecessary links present in the webpage...
 
     if fs[0] in excep :
         continue
 
-
     fs[1] = fs[1].replace(')','')
     print("Date:",fs[1])
-
-    # print(fs[1])
 
     # Replacing some incorrect links with the proper links to direct us to correct webpages...
 
@@ -87,7 +78,7 @@ for link in webpg.find_all('dt'):
         print(addlink)
 
     # Creating text files and inserting content of each President's Addresses...
-    path = 'F:/Cleveland State University/Spring 20/Big Data/Assignment 1/InfoUnionAddress_{}.txt'.format(name)
+    path = 'F:/InfoUnionAddress_{}.txt'.format(name)
     file = open(path,"a+")
 
 
@@ -120,81 +111,57 @@ for link in webpg.find_all('dt'):
 
     # Opening a CSV file and storing all the info of each file into this CSV file for future usage...
 
-    csv_file = "F:/Cleveland State University/Spring 20/Big Data/Assignment 1/state_union_add.csv"
+    csv_file = "F:/state_union_add.csv"
     with open(csv_file, 'a') as state_file:
         file_writer = csv.writer(state_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         file_writer.writerow([fs[0], fs[1], addlink, path, full_text])
 
     # Conneccting to the database......
-    #
-    # conn = pyodbc.connect('Driver={SQL Server};'
-    #                   'Server=LAPTOP-65BJT1KQ;'
-    #                   'Database=nixdb;'
-    #                   'Trusted_Connection=yes;')
-    #
-    # print("Database Connected")
-    #
-    # cursor = conn.cursor()
-    #
-    # cursor.execute('''INSERT INTO nixdb.dbo.StateUnionAddresses(President, [Date of Union Address], [Link to Address], Filename_Address, Text_Address)
-    #                     VALUES(?,?,?,?,?)''', fs[0], fs[1], addlink, path, full_text)
-    #
-    #
-    # cursor.close()
-    #
-    # conn.commit()
-    #
-    # conn.close()
+    conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=XXXXXXXXXX;'
+                      'Database=XXXXX;'
+                      'Trusted_Connection=yes;')
+
+    print("Database Connected")
+
+    cursor = conn.cursor()
+
+    cursor.execute('''INSERT INTO nixdb.dbo.StateUnionAddresses(President, [Date of Union Address], [Link to Address], Filename_Address, Text_Address)
+                        VALUES(?,?,?,?,?)''', fs[0], fs[1], addlink, path, full_text)
+
+
+    cursor.close()
+
+    conn.commit()
+
+    conn.close()
 
     print("Data inserted")
 
     # Creating a big text file for inserting all President's data into a single file with President name and date.....
-    #
-    # bigfile = 'F:/Cleveland State University/Spring 19/Big Data/Assignment 3 Due 4/Website/AllPresidentAddresses.txt'
-    # bigf = open(bigfile,"a+")
-    #
-    # bigf.write(name)
-    # bigf.write("\n")
-    #
-    # for pg in individpg.find_all('p'):
-    #
-    #     stp = pg.find('img')
-    #     start = pg.find('a')
-    #     if start in pg:
-    #         continue;
-    #     if stp in pg:
-    #         break;
-    #
-    #     # print(pg.text)
-    #     fulltxt = pg.text
-    #     bigf.write(fulltxt)
-    #
-    #
-    # bigf.write("\n")
-    #
-    # bigf.close()
-    #
-
-#
-# names,dates,links,filepths,txts=[500],[],[],[],[]
-#
-#
-#
-#
-# with open(csv_file, 'r') as union_file:
-#     readCSV = csv.reader(union_file, delimiter=',')
-#     for row in readCSV:
-#         name=row[0]
-#         date=row[1]
-#         link=row[2]
-#         filepth=row[3]
-#         txt=row[4]
-#
-#         names.append(name)
-#         dates.append(date)
-#         links.append(link)
-#         filepths.append(filepth)
-#         txts.append(txt)
-#
-#     print(names)
+    
+    bigfile = 'F:/AllPresidentAddresses.txt'
+    bigf = open(bigfile,"a+")
+    
+    bigf.write(name)
+    bigf.write("\n")
+    
+    for pg in individpg.find_all('p'):
+    
+        stp = pg.find('img')
+        start = pg.find('a')
+        if start in pg:
+            continue;
+        if stp in pg:
+            break;
+    
+        # print(pg.text)
+        fulltxt = pg.text
+        bigf.write(fulltxt)
+    
+    
+    bigf.write("\n")
+    
+    bigf.close()
+    
